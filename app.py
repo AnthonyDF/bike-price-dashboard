54f3cc266f344b15a9dcc1b43990778b136ac53c
anthonydefaria <defaria.anthony@gmail.co>
2022-09-18 15:07:26 +0200

    average market price implement

--- app.py
+++ app.py
@@ -10,16 +10,18 @@ import dash_bootstrap_components as dbc
 import plotly.graph_objs as go
 from plotly.subplots import make_subplots
 
+import pandas as pd
+
 from utilities.data import get_table
 from utilities.calculation import running_sum
 
 from datetime import datetime, timedelta
 
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.COSMO])
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.SLATE])
 server = app.server
 
 # Import data from postgresql
-filter_date = datetime.today() - timedelta(days=21)
+filter_date = datetime.today() - timedelta(days=40)
 # raw data
 df_raw = get_table("master")
 df_raw = df_raw[df_raw.source.isin(
@@ -30,14 +32,18 @@ df_raw_daily_count = df_raw[['scraped_date', 'source', 'url']].groupby(by=['scra
                                                                        as_index=False).count()
 # clean data
 df_clean_pro = get_table("master_clean_pro")
+
 df_clean_pro_daily_count = df_clean_pro[['scraped_date', 'url']].groupby(by=['scraped_date'], axis=0,
                                                                          as_index=False).count()
+df_clean_pro_daily_price = df_clean_pro[['scraped_date', 'price']].groupby(by=['scraped_date'], axis=0,
+                                                                           as_index=False).mean()
+df_clean_pro_daily_price['SMA30'] = df_clean_pro_daily_price['price'].rolling(30).mean()
 
 df_clean_pro_daily_count['cumul_count'] = df_clean_pro_daily_count.apply(
     lambda x: running_sum(df_clean_pro_daily_count, x.scraped_date), axis=1)
 
-palette = {'red': '#BF3A3A',
-           'green': '#469955'}
+palette = {'red': '#EE553B',
+           'green': '#00CC96'}
 
 
 def gen_fig_daily_spiders():
@@ -59,12 +65,17 @@ def gen_fig_daily_spiders():
                    marker=dict(color=f'{color}')),
             row=k, col=1)
 
-    fig_daily_spiders.update_layout(height=1500, width=1500)
-    fig_daily_spiders.update_layout(showlegend=False)
+    fig_daily_spiders.update_layout(showlegend=False, height=1500,
+                                    template='plotly_dark',
+                                    plot_bgcolor='rgba(0, 0, 0, 0)',
+                                    paper_bgcolor='rgba(0, 0, 0, 0)')
+    fig_daily_spiders.update_yaxes(showgrid=False)
+    fig_daily_spiders.update_xaxes(showgrid=False)
+
     return fig_daily_spiders
 
 
-def gen_fig_daily_master_clean():
+def gen_fig_daily_master_clean_count():
     fig_daily_master_clean = make_subplots(specs=[[{"secondary_y": True}]])
 
     fig_daily_master_clean.add_trace(
@@ -87,32 +98,100 @@ def gen_fig_daily_master_clean():
     fig_daily_master_clean.update_xaxes(title_text="scraped date")
 
     # Set y-axes titles
-    fig_daily_master_clean.update_yaxes(title_text="Cumulated rows", secondary_y=False)
-    fig_daily_master_clean.update_yaxes(title_text="Daily rows", secondary_y=True)
+    fig_daily_master_clean.update_yaxes(secondary_y=False, showgrid=False)
+    fig_daily_master_clean.update_yaxes(secondary_y=True, showgrid=False)
 
     # plotly manual axis adjustments
-    fig_daily_master_clean.update_yaxes(range=[0, 2000], secondary_y=False)
-    fig_daily_master_clean.update_xaxes(range=[datetime(2021, 11, 1), datetime.today()])
+    fig_daily_master_clean.update_yaxes(range=[0, 2000], secondary_y=False, showgrid=False)
+    fig_daily_master_clean.update_xaxes(range=[datetime(2021, 11, 1), datetime.today()], showgrid=False)
+    fig_daily_master_clean.update_layout(template='plotly_dark',
+                                         plot_bgcolor='rgba(0, 0, 0, 0)',
+                                         paper_bgcolor='rgba(0, 0, 0, 0)',
+                                         legend=dict(
+                                             yanchor="top",
+                                             y=0.99,
+                                             xanchor="left",
+                                             x=0.1,
+                                             bgcolor='rgba(0, 0, 0, 0)'
+                                         ))
 
     return fig_daily_master_clean
+
+
+def gen_fig_daily_master_clean_price():
+    fig_daily_master_clean_price = make_subplots(specs=[[{"secondary_y": True}]])
+
+    fig_daily_master_clean_price.add_trace(
+        go.Bar(x=df_clean_pro_daily_price['scraped_date'],
+               y=df_clean_pro_daily_price['price'],
+               name="Daily average price",
+               marker=dict(color=palette['green'])),
+        secondary_y=False
+    )
+
+    fig_daily_master_clean_price.add_trace(
+        go.Scatter(x=df_clean_pro_daily_price['scraped_date'],
+                   y=df_clean_pro_daily_price['SMA30'],
+                   name="Price moving average (30 days)",
+                   mode="lines",
+                   marker=dict(color=palette['red'])),
+        secondary_y=True
+    )
+
+    fig_daily_master_clean_price.update_xaxes(title_text="scraped date")
+
+    # Set y-axes titles
+    fig_daily_master_clean_price.update_yaxes(secondary_y=False, showgrid=False)
+    fig_daily_master_clean_price.update_yaxes(secondary_y=True, showgrid=False)
+
+    # plotly manual axis adjustments
+    fig_daily_master_clean_price.update_yaxes(secondary_y=False, showgrid=False)
+    fig_daily_master_clean_price.update_xaxes(range=[datetime(2021, 11, 1), datetime.today()], showgrid=False)
+    fig_daily_master_clean_price.update_layout(template='plotly_dark',
+                                               plot_bgcolor='rgba(0, 0, 0, 0)',
+                                               paper_bgcolor='rgba(0, 0, 0, 0)',
+                                               legend=dict(
+                                                   yanchor="top",
+                                                   y=0.99,
+                                                   xanchor="left",
+                                                   x=0.1,
+                                                   bgcolor='rgba(0, 0, 0, 0)'
+                                               ))
+
+    return fig_daily_master_clean_price
 
 
 ##########
 # Layout #
 ##########
 
-app.layout = html.Div(children=[
+app.layout = html.Div([
     html.H1('🏍️ Bike price project dashboard'),
-    html.Div([
-        html.H3('📈️ Scraping spiders surveillance'),
-        dcc.Graph(id='fig_daily_spiders', figure=gen_fig_daily_spiders())
-    ]),
-    html.Div([
-        html.H3('💽 Database surveillance'),
-        html.H4('Database size after advanced cleaning'),
-        dcc.Graph(id='fig_daily_master_clean',
-                  figure=gen_fig_daily_master_clean())
-    ])
+    dbc.Container([
+        dbc.Card([
+            dbc.CardBody([
+                html.H3("📈️ Scraping spiders surveillance", className="card-title"),
+                dcc.Graph(id='fig_daily_spiders', figure=gen_fig_daily_spiders())
+            ])
+        ]),
+        html.Br(),
+        dbc.Card([
+            dbc.CardBody([
+                html.H3("💽 Database surveillance", className="card-title"),
+                html.H4("Database size after advanced cleaning", className="card-title"),
+                dcc.Graph(id='fig_daily_master_clean',
+                          figure=gen_fig_daily_master_clean_count())
+            ])
+        ]),
+        html.Br(),
+        dbc.Card([
+            dbc.CardBody([
+                html.H3("💵 Market Price overview (€)", className="card-title"),
+                dcc.Graph(id='fig_daily_master_clean_price',
+                          figure=gen_fig_daily_master_clean_price())
+            ])
+        ])
+    ], fluid=True)
 ])
 
 if __name__ == "__main__":
